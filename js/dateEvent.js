a758b4d6e1e57c1d945a3f38bd901191b45ac37a
stewardzhao <stewardzhao@gmail.com>
2016-11-28 23:16:18 -0800

    ss

---
+++ js/dateEvent.js
@@ -0,0 +1,7 @@
+$(function () {
+            $('#myCalendar').jalendar({
+                customDay: '2016/12/24',  // Format: Year/Month/Day
+                color: '#ba968d', // Unlimited Colors
+                lang: 'EN' // Format: English — 'EN'
+            });
+});
